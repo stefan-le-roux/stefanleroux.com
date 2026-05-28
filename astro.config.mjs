63d78d701247660234c8ba33312e80432b14a574
Stefan le Roux <stefan@vivtec.co.za>
2026-05-28 14:48:46 +0200

    astro 4.6.0 update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { satteri } from "@astrojs/markdown-satteri";
 
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
 	site: "https://stefanleroux.com",
+	markdown: {
+		processor: satteri({
+			features: { directive: true },
+		}),
+	},
 	integrations: [mdx(), sitemap()],
 	trailingSlash: "always",
 });
