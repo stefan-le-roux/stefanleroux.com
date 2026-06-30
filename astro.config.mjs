78f948cfec001ee80ac6efce7243bda20e9cc250
Stefan le Roux <stefan@vivtec.co.za>
2026-06-30 14:10:11 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://stefanleroux.com",
+	base: "/",
+	trailingSlash: "always",
 	integrations: [mdx(), sitemap()],
 });
